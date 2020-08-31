d0bc989d460700ceacac514078d26f9a09400943
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-08-31 14:35:02 -0700

    Added util function to color item icon
    Also properly enclosed lua tables with (C const) int keys

--- EasyRollTracker/EasyRollTracker.lua
+++ EasyRollTracker/EasyRollTracker.lua
@@ -50,18 +50,18 @@ local function Colorize(text, color)
 end
 
 local const_classcolor = {
-	DEATHKNIGHT	= "C41F3B",
-	DEMONHUNTER	= "A330C9",
-	DRUID		= "FF7D0A",
-	HUNTER		= "A9D271",
-	MAGE		= "40C7EB",
-	MONK		= "00FF96",
-	PALADIN		= "F58CBA",
-	PRIEST		= "FFFFFF",
-	ROGUE		= "FFF569",
-	SHAMAN		= "0070DE",
-	WARLOCK		= "8787ED",
-	WARRIOR		= "C79C6E",
+	[DEATHKNIGHT]	= "C41F3B",
+	[DEMONHUNTER]	= "A330C9",
+	[DRUID]			= "FF7D0A",
+	[HUNTER]		= "A9D271",
+	[MAGE]			= "40C7EB",
+	[MONK]			= "00FF96",
+	[PALADIN]		= "F58CBA",
+	[PRIEST]		= "FFFFFF",
+	[ROGUE]			= "FFF569",
+	[SHAMAN]		= "0070DE",
+	[WARLOCK]		= "8787ED",
+	[WARRIOR]		= "C79C6E",
 }
 local function ColorizeName(name)
 	local classname, _ = UnitClassBase(name)
@@ -69,11 +69,25 @@ local function ColorizeName(name)
 	return Colorize(name, color)
 end
 
+local const_raritycolor = {
+	[LE_ITEM_QUALITY_POOR]		= {0.6157, 0.6157, 0.6157},
+	[LE_ITEM_QUALITY_COMMON]	= {1.0000, 1.0000, 1.0000},
+	[LE_ITEM_QUALITY_UNCOMMON]	= {0.1176, 1.0000, 0.0000},
+	[LE_ITEM_QUALITY_RARE]		= {0.0000, 0.4392, 0.8667},
+	[LE_ITEM_QUALITY_EPIC]		= {0.6392, 0.2078, 0.9333},
+	[LE_ITEM_QUALITY_LEGENDARY]	= {1.0000, 0.5020, 0.0000},
+	[LE_ITEM_QUALITY_ARTIFACT]	= {0.9020, 0.8000, 0.5020},
+	[LE_ITEM_QUALITY_HEIRLOOM]	= {0.0000, 0.8000, 1.0000}
+}
+local function ColorizeLayer(frame, rarity)
+	frame:SetVertexColor(unpack(const_raritycolor[rarity]))
+end
+
 local const_roleicon = {
-	TANK	= CreateAtlasMarkup("roleicon-tiny-tank"),
-	HEALER	= CreateAtlasMarkup("roleicon-tiny-healer"),
-	DAMAGER	= CreateAtlasMarkup("roleicon-tiny-dps"),
-	NONE	= CreateAtlasMarkup("roleicon-tiny-none"),
+	[TANK]		= CreateAtlasMarkup("roleicon-tiny-tank"),
+	[HEALER]	= CreateAtlasMarkup("roleicon-tiny-healer"),
+	[DAMAGER]	= CreateAtlasMarkup("roleicon-tiny-dps"),
+	[NONE]		= CreateAtlasMarkup("roleicon-tiny-none"),
 }
 local function RoleIconString(name)
 	local role = UnitGroupRolesAssigned(name)
