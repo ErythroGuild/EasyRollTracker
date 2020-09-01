e65d164925ef67058e829ae0d9933c0022531616
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-09-01 16:26:45 -0700

    Added scrollbar thumb size adjustment code
    implementation has some bugs: #53, #54

--- EasyRollTracker/EasyRollTracker.lua
+++ EasyRollTracker/EasyRollTracker.lua
@@ -303,12 +303,23 @@ function eRollTracker_ClearAll()
 	eRollTracker.entries = {}
 	eRollTrackerFrame_Scroll_Layout:Layout()
 end
--- group_scroll:SetLayout("Fill")
--- ui:AddChild(group_scroll)
 
--- local scrollFrame_main = AceGUI:Create("ScrollFrame")
--- scrollFrame_main:SetLayout("List")
--- group_scroll:AddChild(scrollFrame_main)
+function eRollTrackerFrame_Scroll_OnScrollRangeChanged(self, xrange, yrange)
+	local name = self:GetName();
+	local scrollbar = self.ScrollBar or _G[name.."ScrollBar"];
+	local thumbTexture = scrollbar.ThumbTexture or _G[scrollbar:GetName().."ThumbTexture"];
+
+	local range_window = self:GetHeight()
+	local range_max = self:GetScrollChild():GetHeight()
+	local height_bar = scrollbar:GetHeight()
+	local height_thumb = height_bar * (range_window / range_max)
+	height_thumb = math.min(height_thumb, height_bar)
+	height_thumb = math.max(height_thumb, 18)
+	
+	thumbTexture:SetHeight(height_thumb)
+
+	ScrollFrame_OnScrollRangeChanged(self, xrange, yrange)
+end
 
 -- -- Define event listeners.
 -- function newEntry(player, roll, max)
